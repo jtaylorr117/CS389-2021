0a4a6d9aef7774c707ba06b73735f653ae67a24f
jtaylorr117 <jt10334n@pace.edu>
2021-03-10 00:33:33 -0500

    finished android_project_2

---
+++ android_project_2/unit_2/PhoneNumberSpinnerEspresso/app/src/androidTest/java/com/example/android/phonenumberspinner/SpinnerSelectionTest.java
@@ -0,0 +1,51 @@
+
+package com.example.android.phonenumberspinner;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(AndroidJUnit4.class)
+public class SpinnerSelectionTest {
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.android.phonenumberspinner",
+                appContext.getPackageName());
+    }
+
+
+    @Test
+    public void iterateSpinnerItems() {
+        String[] myArray = mActivityRule.getActivity().getResources()
+                .getStringArray(R.array.labels_array);
+        int size = myArray.length;
+        for (int i=0; i<size; i++) {
+            onView(withId(R.id.label_spinner)).perform(click());
+            onData(is(myArray[i])).perform(click());
+            onView(withId(R.id.text_phonelabel))
+                    .check(matches(withText(containsString(myArray[i]))));
+        }
+    }
+}
---
+++ android_project_2/unit_2/PickerForTime/app/src/main/java/com/example/android/pickerfortime/MainActivity.java
@@ -0,0 +1,35 @@
+
+package com.example.android.pickerfortime;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePicker(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.timepicker));
+    }
+
+    public void processTimePickerResult(int hourOfDay, int minute) {
+        // Convert time elements into strings.
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        // Assign the concatenated strings to timeMessage.
+        String timeMessage = (hour_string + ":" + minute_string);
+
+        Toast.makeText(this,getString(R.string.time_toast)
+                + timeMessage, Toast.LENGTH_SHORT).show();
+    }
+}
